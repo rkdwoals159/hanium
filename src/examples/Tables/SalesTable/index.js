b220f0fe934fc5da14b20f4a9896fd598bd74ae3
rkdwoals159 <rkdwoals159@gmail.com>
2023-09-20 00:02:43 +0900

    fix : 리스트업스크롤 구현 및  css 커스터마이징

--- src/examples/Tables/SalesTable/index.js
+++ src/examples/Tables/SalesTable/index.js
@@ -1,59 +1,46 @@
-/**
-=========================================================
-* Argon Dashboard 2 MUI - v3.0.1
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/argon-dashboard-material-ui
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-
-Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
-import { useMemo } from "react";
-
-// prop-types is a library for typechecking of props
+import { useEffect, useMemo, useRef } from "react";
 import PropTypes from "prop-types";
-
-// @mui material components
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-
-// Argon Dashboard 2 MUI components
 import ArgonTypography from "components/ArgonTypography";
 import ArgonBox from "components/ArgonBox";
-
-// Argon Dashboard 2 MUI example components
 import SalesTableCell from "examples/Tables/SalesTable/SalesTableCell";
+import { TableCell } from "@mui/material";
 
 function SalesTable({ socketData, title }) {
-  const renderTableCells = socketData ? socketData.map((data, key) => {
-    const rowKey = `row-${key}`;
-    const cellKey = [`cell-${key}-1`,`cell-${key}-2`];
-    const tableRows = [
-      <SalesTableCell
-      key={cellKey[0]}
-      title={"Uuid"}
-      content={data.uuid}
-    />,
-    <SalesTableCell
-    key={cellKey[1]}
-    title={"결과"}
-    content={data.anomal_TF ? "정상" : "비정상"}
-  />
-    ]
-    
-    return <TableRow key={rowKey}>{tableRows}</TableRow>;
-  }) : [];
+  const containerRef = useRef(null);
+
+  // 속성 또는 상태가 변경될 때 스크롤을 맨 아래로 이동
+  useEffect(() => {
+    if (containerRef.current) {
+      containerRef.current.scrollTop = containerRef.current.scrollHeight;
+    }
+  }, [socketData]);
+  const renderTableCells = socketData
+    ? socketData.map((data, key) => {
+        const rowKey = `row-${key}`;
+        const cellKey = [`cell-${key}-1`, `cell-${key}-2`];
+        const tableRows = [
+          <SalesTableCell key={`cell-${key}-1`} title={"Uuid"} content={data.uuid} />,
+          <SalesTableCell key={`cell-${key}-2`} title={"결과"} content={data.anomal_TF ? "정상" : "비정상"} />,
+          <TableCell TableCell key={`cell-${key}-3`} align="center">
+            <ArgonBox display="flex" flexDirection="column">
+              <ArgonTypography variant="button" fontWeight="medium" color="warning" textTransform="capitalize">
+                상세 보기
+              </ArgonTypography>
+            </ArgonBox>
+          </TableCell>,
+        ];
+
+        return <TableRow key={rowKey}>{tableRows}</TableRow>;
+      })
+    : [];
 
   return (
-    <TableContainer sx={{ height: "100%" }}>
+    <TableContainer ref={containerRef} sx={{ height: "1000px", overflowY: "auto" }}> {/* 여기서 maxHeight를 조절하여 최대 높이를 변경할 수 있습니다. */}
       <Table>
         <TableHead>
           <ArgonBox component="tr" width="max-content" display="block" mb={1.5}>
@@ -68,15 +55,13 @@ function SalesTable({ socketData, title }) {
   );
 }
 
-// Setting default values for the props of SalesTable
 SalesTable.defaultProps = {
   socketData: [],
 };
 
-// Typechecking props for the SalesTable
 SalesTable.propTypes = {
   title: PropTypes.string.isRequired,
-  socketData : PropTypes.arrayOf(PropTypes.object),
+  socketData: PropTypes.arrayOf(PropTypes.object),
 };
 
 export default SalesTable;
